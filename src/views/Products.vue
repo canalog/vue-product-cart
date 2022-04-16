7c7b6b2ee0a620335aa9592879f0820c66d3c100
canalog <alicecha719@naver.com>
2022-04-17 08:51:01 +0900

    Vue CLI 프로젝트로 변환

---
+++ src/views/Products.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="wrapper">
+    <h1>Products</h1>
+
+    <div class="card-container">
+       <ProductCard
+        v-for="(product, index) in inventory.slice(0, 6)"
+        :key="product.id"
+        :product="product"
+        :addToCart="addToCart"
+        :index="index"
+        class="card"
+      />
+    </div>
+  </main>
+</template>
+
+<script>
+import ProductCard from "@/components/ProductCard.vue"
+
+export default {
+  props: ["inventory", "addToCart"],
+  components: {
+    ProductCard
+  }
+}
+</script>
